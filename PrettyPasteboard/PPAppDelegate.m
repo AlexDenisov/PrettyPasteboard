52d239687a84ef42ffb92f1d6451e4a493ce3942
AlexDenisov <1101.debian@gmail.com>
2014-06-01 22:33:08 +0300

    Added 'Enabled' menu item

--- PrettyPasteboard/PPAppDelegate.m
+++ PrettyPasteboard/PPAppDelegate.m
@@ -14,20 +14,46 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    [self initStatusBar];
+    [self initMenu];
+    [self initObserver];
+}
+
+- (void)initStatusBar
+{
     NSStatusBar *statusBar = [NSStatusBar systemStatusBar];
     self.statusBarIcon = [statusBar statusItemWithLength:NSSquareStatusItemLength];
     self.statusBarIcon.title = @"PP";
-    
+}
+
+- (void)initMenu
+{
     NSMenu *menu = [NSMenu new];
+    NSMenuItem *enableItem = [menu addItemWithTitle:@"Enabled" action:@selector(changeState:) keyEquivalent:@""];
+    [enableItem setState:1];
     [menu addItem:[NSMenuItem separatorItem]];
     [menu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@""];
     self.statusBarIcon.menu = menu;
-    
+}
+
+- (void)initObserver
+{
     self.observer = [PPPasteboardObserver new];
     
     PPPasteboardJSONHandler *jsonHandler = [PPPasteboardJSONHandler new];
     [self.observer addSubscriber:jsonHandler];
     [self.observer startObserving];
+}
+
+- (void)changeState:(NSMenuItem *)sender
+{
+    if (sender.state) {
+        [self.observer stopObserving];
+    } else {
+        [self.observer startObserving];
+    }
+    
+    sender.state = !sender.state;
 }
 
 @end
